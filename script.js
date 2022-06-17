1c4b19c24a03864621bb34ece707a03e81fb016a
pedro <pedro.henrike2812@gmail.com>
2022-06-17 13:37:12 -0300

    Finalizado Layout

---
+++ script.js
@@ -0,0 +1,37 @@
+// CRIANDO VARIÁVEL PARA CARD-BODY RECEBER SEUS FILHOS
+let cardBody = document.querySelector('#cardbody')
+
+async function adicionarDados(){
+  const url = 'https://swapi.dev/api/starships/'
+
+
+  try {
+    const resultado = await axios.get(url)
+    console.log(resultado.data.results)
+    const dados = resultado.data.results
+    
+
+    // CRIANDO ELEMENTOS HTML DINAMICAMENTE
+    for(elementos of dados){ 
+
+      // CRIANDO TÍTULO DO CARD
+      const titulo = document.createElement('h5')
+      titulo.classList.add('card-title')
+      titulo.textContent = elementos.name
+
+      // CRIANDO PARÁGRAFO DO CARD ( ONDE IRÁ O TEXTO )
+      const texto = document.createElement('p')
+      texto.classList.add('card-text')
+      texto.textContent = `Modelo: ${elementos.model}`
+      // ENVIANDO AS TAG'S FILHAS, PARA O PAI CARD-BODY
+      cardBody.appendChild(titulo)
+      cardBody.append(texto)
+    }
+    
+
+  } catch (error) {
+    console.log('${error}')
+  }
+}
+
+adicionarDados()
